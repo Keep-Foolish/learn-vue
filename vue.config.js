6585ce32e0467120bd3a1fd500632fd4d2ed5af0
张伟 <zhangwei@zhangweideMacBook-Pro.local>
2022-07-05 17:39:17 +0800

    publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/vue-github-page/',
+  publicPath: '/learn-vue/',
   lintOnSave: false
 })
